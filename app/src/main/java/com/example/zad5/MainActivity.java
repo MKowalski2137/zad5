6c5de9a499479bba82bcf96a16e6c951f5d52fc2
MKowalski2137 <markowgithub@gmail.com>
2024-12-10 13:03:15 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/zad5/MainActivity.java
+++ app/src/main/java/com/example/zad5/MainActivity.java
@@ -1,24 +1,44 @@
-package com.example.zad5;
+package com.example.domekwgrach;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int licznikpolub = 0;
+    private TextView licznikpolubtext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        licznikpolubtext = findViewById(R.id.licznikpolub);
+        Button polub = findViewById(R.id.polub);
+        Button usun = findViewById(R.id.usun);
+
+        polub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                licznikpolub++;
+                updateLikeCount();
+            }
         });
+
+        usun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (licznikpolub > 0) {
+                    licznikpolub--;
+                    updateLikeCount();
+                }
+            }
+        });
+    }
+    private void updateLikeCount() {
+        licznikpolubtext.setText(licznikpolub + " polubień");
     }
 }
